78c4e5dc5b5bbbf934185d1bf0cad00c4218094a
Waleed Mubasher <moomanmubasher656@gmail.com>
2024-03-21 19:57:07 +0500

    token and default admin

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,11 +1,38 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const user = require("../models/user.model");
 
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
     .then(() => {
+        
         console.log("Connected");
+        user.findOne({ email: 'admin@gmail.com' }, (err, admin) => {
+            if (err) {
+              console.error('Error checking admin user:', err);
+              return;
+            }
+      
+            if (!admin) {
+              // Create admin user
+              const newAdmin = new user({
+                email: 'admin@gmail.com',
+                password: 'admin123', // Use secure password hashing in a real-world scenario
+                role: 'admin',
+              });
+      
+              newAdmin.save((err, savedAdmin) => {
+                if (err) {
+                  console.error('Error creating admin user:', err);
+                  return;
+                }
+                console.log('Admin user created:', savedAdmin);
+              });
+            } else {
+              console.log('Admin user already exists:');
+            }
+          });
     })
     .catch((err) => {
         console.log("Error In DB Connection : ", err);
--- src/middlewares/tokenVerfiy.js
+++ src/middlewares/tokenVerfiy.js
@@ -4,11 +4,12 @@ const secretKey = "abc123"
 // Middleware to verify JWT
 const verifyToken = (req, res, next) => {
     const token = req.headers['authorization'];
-    if (!token) {
+    const tokenWithoutBearer = token.substring(7);
+    if (!tokenWithoutBearer) {
         return res.status(401).send('Access denied. Token is required');
     }
     try {
-        const decoded = jwt.verify(token, secretKey);
+        const decoded = jwt.verify(tokenWithoutBearer, secretKey);
         req.userId = decoded.userId;
         next();
     } catch (error) {
